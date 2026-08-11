ca92969605073840a52d5eaacb70e1ccc48643f7
anastasya <nastabutcher.myasn@gmail.com>
2026-08-11 15:21:53 +0500

    добавлен плагин jacoco
    генерируется отчет по покрытию тестами всех слоев chapter.
    установлен минимальный порог покрытия строк - 80%

--- src/test/java/ru/anastasya/readingportal/ReadingportalApplicationTests.java
+++ src/test/java/ru/anastasya/readingportal/ReadingportalApplicationTests.java
@@ -2,9 +2,18 @@ package ru.anastasya.readingportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ReadingportalApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
